7fda9370d8ddd37fd4c547b8027fff6360a5b060
Gilang <gilangphb@gmail.com>
2022-11-19 14:34:17 +0700

    Menambahkan Utama.java

---
+++ Utama.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.latihan1;
+
+/**
+ *
+ * @author hp
+ */
+public class Utama {
+    
+    public static void main(String[] args) {
+    
+        Programmer Andi = new Programmer("Andi");
+        Programmer Riko= new Programmer("Riko", 6000000);
+        Programmer Dina= new Programmer("Riko", 6000000, 3000000);
+        
+    }
+}
